c9acc0b6de77f6828bfc189c08867148814fb9b3
DixzzXD88 <rf3056098@gmail.com>
2026-07-01 09:43:57 +0700

    Increase loop range from 12 to 13

--- installprotect.sh
+++ installprotect.sh
@@ -4,8 +4,7 @@ echo "========================================="
 echo "  Running all protect scripts..."
 echo "========================================="
 
-# Loop dari 1 sampai 12
-for i in {1..12}; do
+for i in {1..13}; do
     if [ -f "protect$i.sh" ]; then
         echo "▶️  Running protect$i.sh..."
         bash protect$i.sh
